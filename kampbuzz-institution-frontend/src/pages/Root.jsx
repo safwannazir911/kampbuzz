fac4cea0b9739ec0c5acf4c0f9e981c6bdd79541
safwannazir911 <safwannazir911@gmail.com>
2025-06-28 18:59:36 +0530

    refactor: Auth pages

--- kampbuzz-institution-frontend/src/pages/Root.jsx
+++ kampbuzz-institution-frontend/src/pages/Root.jsx
@@ -2,11 +2,11 @@ import useAuthUser from "react-auth-kit/hooks/useAuthUser";
 import Login from "./Login";
 import Signup from "./Signup";
 import { UserPlus, LogIn } from "lucide-react";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Header } from "@/components/Header";
 import dashLogBg from "@/assets/dashLogBg.png";
-import GoogleLogo from "@/assets/GoogleLogo.png";
+import { Button } from "@/components/ui/button";
 
 const Root = () => {
   const authUser = useAuthUser();
@@ -29,7 +29,7 @@ const Root = () => {
       >
         <div className="md:w-[50%] flex md:min-h-[100%] justify-center items-start md:pt-[5em] md:p-2 p-10">
           <div className="flex flex-col items-start">
-            <p className="md:text-3xl text-xl font-bold">Get To</p>
+            <p className="md:text-3xl text-xl font-bold">Welcome to</p>
             <p className="md:text-5xl text-3xl font-bold">Your Dashboard</p>
             <p className="md:text-xl  text-[14px] font-light">
               Time to work your magic and keep things in check on the dashboard.
@@ -40,9 +40,9 @@ const Root = () => {
           {isLogin ? <Login /> : <Signup />}
 
           <div className="flex justify-center mt-4">
-            <button
+            <Button
               onClick={() => setIsLogin(!isLogin)}
-              className="flex items-center px-4 py-2 bg-purple-600 text-white rounded-md hover:bg-purple-700 transition-colors"
+              variant="outline"
             >
               {isLogin ? (
                 <>
@@ -55,8 +55,22 @@ const Root = () => {
                   Go to Login
                 </>
               )}
-            </button>
+            </Button>
           </div>
+
+          <div className="flex items-center justify-center mt-4">
+            <Button variant="outline" >
+              <a href="http://kb-author.vercel.app" target="_blank">
+                Author Login
+              </a>
+            </Button>
+            <Button variant="outline" className="ml-2">
+              <a href="http://kb-publisher.vercel.app" target="_blank" >
+                Publisher Login
+              </a>
+            </Button>
+          </div>
+
 
         </div>
 
